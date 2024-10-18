9a1b2b9fdb5a5425085ad306979c2ddc8eee2f7f
keke125 <admin@keke125.com>
2024-10-18 22:50:33 +0800

    Initial commit

---
+++ src/main/java/com/keke125/vaultguardweb/VaultGuardWebApplication.java
@@ -0,0 +1,13 @@
+package com.keke125.vaultguardweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultGuardWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaultGuardWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/keke125/vaultguardweb/VaultGuardWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keke125.vaultguardweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultGuardWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
