e18abf0ece59cf1f63ff8b40b27e0d28939698cc
keke125 <admin@keke125.com>
2024-10-29 21:16:44 +0800

    fix: Modify API return message for clarity.

--- src/main/java/com/keke125/vaultguard/web/spring/password/ResponseMessage.java
+++ src/main/java/com/keke125/vaultguard/web/spring/password/ResponseMessage.java
@@ -4,11 +4,11 @@ import java.util.Collections;
 import java.util.Map;
 
 public class ResponseMessage {
-    public static String successSavePasswordMessage = "密碼儲存成功";
+    public static String successSavePasswordMessage = "成功儲存密碼";
     public static String passwordDuplicatedMessage = "已有相同名稱及帳號的密碼!";
     public static String passwordNotFoundMessage = "找不到密碼!";
-    public static String successUpdatePasswordMessage = "密碼更新成功";
-    public static String successDeletePasswordMessage = "密碼刪除成功";
+    public static String successUpdatePasswordMessage = "成功更新密碼";
+    public static String successDeletePasswordMessage = "成功刪除密碼";
     public static Map<String, String> successSavePasswordResponse = Collections.singletonMap("message", successSavePasswordMessage);
     public static Map<String, String> passwordDuplicatedResponse = Collections.singletonMap("message", passwordDuplicatedMessage);
     public static Map<String, String> passwordNotFoundResponse = Collections.singletonMap("message", passwordNotFoundMessage);
