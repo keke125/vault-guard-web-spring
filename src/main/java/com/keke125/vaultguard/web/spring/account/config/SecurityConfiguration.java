e00ad23cf285dffb2e4855a2da0c5bd3eed727cc
keke125 <admin@keke125.com>
2024-10-29 20:32:05 +0800

    refactor: Remove unused code.

--- src/main/java/com/keke125/vaultguard/web/spring/account/config/SecurityConfiguration.java
+++ src/main/java/com/keke125/vaultguard/web/spring/account/config/SecurityConfiguration.java
@@ -17,13 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.CorsUtils;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @EnableWebSecurity
