6b93d94ed2b502f4edb51f39fd65af357128fa7e
keke125 <admin@keke125.com>
2024-12-28 00:38:53 +0800

    refactor: Remove unused code.

--- src/main/java/com/keke125/vaultguard/web/spring/util/DelegatedAuthenticationEntryPoint.java
+++ src/main/java/com/keke125/vaultguard/web/spring/util/DelegatedAuthenticationEntryPoint.java
@@ -1,19 +1,16 @@
 package com.keke125.vaultguard.web.spring.util;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
 @Component
 public class DelegatedAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
