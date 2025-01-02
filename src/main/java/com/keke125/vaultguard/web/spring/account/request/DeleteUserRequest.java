deecd2227fcb3a06ede081c5eb5a13f349ac36fd
keke125 <admin@keke125.com>
2025-01-03 03:51:01 +0800

    refactor: Remove unused code.

--- src/main/java/com/keke125/vaultguard/web/spring/account/request/DeleteUserRequest.java
+++ src/main/java/com/keke125/vaultguard/web/spring/account/request/DeleteUserRequest.java
@@ -1,6 +1,5 @@
 package com.keke125.vaultguard.web.spring.account.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
